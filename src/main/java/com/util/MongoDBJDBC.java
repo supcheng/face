aec7eb6d6bff4cbaa1da62bc90d9e9932826c69e
cheng <1127887913@qq.com>
2021-04-09 15:52:19 +0800

    整合

---
+++ src/main/java/com/util/MongoDBJDBC.java
@@ -0,0 +1,56 @@
+package com.util;
+
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongoDBJDBC {
+    public static void main( String args[] ){
+        try{
+            // 连接到 mongodb 服务
+            MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+
+            // 连接到数据库
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("runnoob");
+            System.out.println("Connect to database successfully");
+            MongoCollection<Document> collection = mongoDatabase.getCollection("runnoob");
+            System.out.println("集合 test 选择成功");
+
+
+//            Document document = new Document("title", "MongoDB").
+//                    append("description", "database").
+//                    append("likes", 100).
+//                    append("by", "Fly");
+//            List<Document> documents = new ArrayList<Document>();
+//            documents.add(document);
+//            collection.insertMany(documents);
+//            System.out.println("文档插入成功");
+
+
+//            FindIterable<Document> findIterable = collection.find();
+//            MongoCursor<Document> mongoCursor = findIterable.iterator();
+//            while(mongoCursor.hasNext()){
+//                System.out.println(mongoCursor.next());
+//            }
+
+            //更新文档   将文档中likes=100的文档修改为likes=200
+            collection.updateMany(Filters.eq("likes", 100), new Document("$set",new Document("likes",200)));
+            //检索查看结果
+            FindIterable<Document> findIterable = collection.find();
+            MongoCursor<Document> mongoCursor = findIterable.iterator();
+            while(mongoCursor.hasNext()){
+                System.out.println(mongoCursor.next());
+            }
+        }catch(Exception e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+    }
+}
